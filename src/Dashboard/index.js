08840b59fe0db9a9e6add5f6a9687a04c17f8730
juned-antino <juned.a@antino.io>
2022-06-28 18:37:53 +0530

    create a dashboard

---
+++ src/Dashboard/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import LeftNav from "../components/leftNav";
+import TopNav from "../components/topNav";
+import Dashboard from "./Dashboard";
+
+const DashboardMain = () => {
+    return (
+        <>
+            <TopNav />
+            <div style={{ display: "flex", background: "#FFE3D2" }}>
+                <LeftNav />
+                <Dashboard />
+            </div>
+        </>
+    );
+};
+
+export default DashboardMain;
